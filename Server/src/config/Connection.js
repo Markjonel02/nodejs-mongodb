98d70b90c6fe802ea48076f736180c4c4f1cbb5e
Markjonel02 <rellesmarkjonel14@gmail.com>
2025-06-18 13:30:33 +0800

    working on atlas

--- Server/src/config/Connection.js
+++ Server/src/config/Connection.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+/* const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = async () => {
-  const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/NoteApplication";
+  const MONGO_URI = process.env.MONGO_URI;
 
   if (!MONGO_URI) {
     console.error("MongoDB connection error: MONGO_URI is not defined.");
@@ -18,3 +18,38 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+ */
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+if (!uri) {
+  console.error("MongoDB connection error: MONGO_URI is not defined.");
+  process.exit(1);
+}
+
+// Create a MongoClient with Stable API settings
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectDB() {
+  try {
+    // Connect to MongoDB Atlas
+    await client.connect();
+    await client.db("NoteApplication").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB Atlas!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
+
+module.exports = { client, connectDB };
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const ConnectDB = require("./config/Connection");
+const { connectDB } = require("./config/Connection");
+
 const dotenv = require("dotenv");
 dotenv.config(); // Load environment variables from .env file
 
-ConnectDB(); // Connect to MongoDB
+connectDB(); // Connect to MongoDB
 // Load environment variables
 const app = express();
-const port = /* process.env.PORT ||  */ 5000; // Use PORT from .env or default to 5000
+const port = process.env.PORT; // Use PORT from .env or default to 5000
 
 // Middleware
 app.use(express.json());
@@ -37,6 +38,17 @@ app.delete("/api", (req, res) => {
   res.send("Moved to archived Successfully");
 }); */
 
-app.listen(port, () => {
+/* app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+ */
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
