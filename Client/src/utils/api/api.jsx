8441ca70609cb33b4315c4c12cf46d42cc786376
Markjonel02 <rellesmarkjonel14@gmail.com>
2025-06-29 21:40:52 +0800

    modified:   Client/src/utils/api/api.jsx

--- Client/src/utils/api/api.jsx
+++ Client/src/utils/api/api.jsx
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 const VITE_API_BACKEND_URL =
-  import.meta.env.MODE === "production"
-    ? "https://nodejs-mongodb-79vz.onrender.com"
-    : "http://localhost:5000";
-// This is correct.
-// In dev: http://localhost:5000
-// In prod (Vercel): /api (which means requests go to https://your-vercel-app.com/api/...)
+  "https://nodejs-mongodb-79vz.onrender.com" || "http://localhost:5000";
 
+// Export the configured Axios instance
 export const api = axios.create({
   baseURL: VITE_API_BACKEND_URL,
+  withCredentials: true, // Important for sending cookies/auth headers in cross-origin requests
 });
